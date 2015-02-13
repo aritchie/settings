11668fe30657d1614ec6d65bbcbfcc4322613a01
Allan Ritchie <allan.ritchie@gmail.com>
2015-02-12 21:50:16 -0500

    Add init for stupid linkers

--- src/Acr.Settings.Shared/Settings.cs
+++ src/Acr.Settings.Shared/Settings.cs
@@ -5,25 +5,16 @@ namespace Acr.Settings {
 
     public static class Settings {
         private static ISettings instance;
-        private static readonly object syncLock = new object();
 
 
-        public static ISettings Instance {
-            get {
-                if (instance == null) {
-                    lock (syncLock) {
-                        if (instance == null) {
-#if __PLATFORM__
-                            instance = new SettingsImpl();
-#else
-                            throw new Exception("Platform implementation not found.  Have you added a nuget reference to your platform project?");
-#endif
-                        }
-                    }
-                }
-                return instance;
-            }
-            set { instance = value; }
+        public static void Init() {
+            #if __PLATFORM__
+            instance = new SettingsImpl();
+            #else
+            throw new Exception("Platform implementation not found.  Have you added a nuget reference to your platform project?");
+            #endif
         }
+
+        public static ISettings Instance { get; set; }
     }
 }
