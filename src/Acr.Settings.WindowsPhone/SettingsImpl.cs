9cabf13b98163c5dc43f2a8f379f625c8a3a909c
Allan Ritchie <allan.ritchie@gmail.com>
2015-06-16 07:55:52 -0400

    Abstraction library Fix for wp8 and wp81

---
+++ src/Acr.Settings.WindowsPhone/SettingsImpl.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Windows.Storage;
+
+
+namespace Acr.Settings {
+
+    public class SettingsImpl : AbstractSettings {
+        private readonly ApplicationDataContainer container;
+
+
+        public SettingsImpl(bool isRoaming = false) {
+            this.IsRoamingProfile = isRoaming;
+            this.container = this.IsRoamingProfile
+                ? ApplicationData.Current.RoamingSettings
+                : ApplicationData.Current.LocalSettings;
+        }
+
+
+        public override bool Contains(string key) {
+            return this.container.Values.ContainsKey(key);
+        }
+
+
+        protected override void NativeClear() {
+            var keys = this.container
+                .Values
+                .Where(x => this.ShouldClear(x.Key))
+                .Select(x => x.Key)
+                .ToList();
+
+            foreach (var key in keys)
+                this.container.Values.Remove(key);
+        }
+
+
+        protected override object NativeGet(Type type, string key) {
+            return this.container.Values[key];
+        }
+
+
+        protected override void NativeSet(Type type, string key, object value) {
+            this.container.Values[key] = value;
+        }
+
+
+        protected override void NativeRemove(string key) {
+            this.container.Values.Remove(key);
+        }
+
+
+        protected override IDictionary<string, string> NativeValues() {
+            return this.container
+                .Values
+                .ToDictionary(
+                    x => x.Key,
+                    x => x.Value.ToString()
+                );
+        }
+    }
+}
