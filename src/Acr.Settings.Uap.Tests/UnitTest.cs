9c0d25e8308bb42301e5ffab4620a3d3feb9f33a
Allan Ritchie <allan.ritchie@gmail.com>
2015-08-16 10:36:40 -0400

    4.3 release

---
+++ src/Acr.Settings.Uap.Tests/UnitTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+
+namespace Acr.Settings.Uap.Tests {
+
+    [TestClass]
+    public class UnitTest1 {
+
+        [TestMethod]
+        public void TestMethod1() {
+        }
+    }
+}
