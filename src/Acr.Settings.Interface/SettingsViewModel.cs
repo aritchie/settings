519d6d32a3c9a8958790e680a22b695e3564e77c
Allan Ritchie <allan.ritchie@gmail.com>
2015-10-13 07:46:21 -0400

    settings viewmodel

---
+++ src/Acr.Settings.Interface/SettingsViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+
+namespace Acr.Settings {
+
+    public class SettingsViewModel : INotifyPropertyChanged {
+        public event PropertyChangedEventHandler PropertyChanged;
+        readonly ISettings settings;
+
+
+        public SettingsViewModel(ISettings settings) {
+            this.settings = settings;
+            this.settings.Changed += (sender, args) => this.OnPropertyChanged(args.Key);
+        }
+
+
+        protected virtual bool SetProperty<T>(T value, [CallerMemberName] string propertyName = null) {
+            var current = this.settings.Get<T>(propertyName);
+            if (Object.Equals(current, value))
+                return false;
+
+            this.settings.Set(propertyName, value);
+            this.OnPropertyChanged(propertyName);
+            return true;
+        }
+
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null) {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
