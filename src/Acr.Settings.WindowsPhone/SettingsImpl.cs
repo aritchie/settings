f0908aace27bbf6a5e4e6059c4e09472736be2c5
Allan Ritchie <allan.ritchie@gmail.com>
2015-10-21 21:48:29 -0400

    fix #2 - need to add unit tests for universal again.  need to find way to unit test wp8-silverlight as well

--- src/Acr.Settings.WindowsPhone/SettingsImpl.cs
+++ src/Acr.Settings.WindowsPhone/SettingsImpl.cs
@@ -1,62 +1,57 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO.IsolatedStorage;
 using System.Linq;
-using Windows.Storage;
 
 
 namespace Acr.Settings {
 
     public class SettingsImpl : AbstractSettings {
-        readonly ApplicationDataContainer container;
+        readonly IsolatedStorageSettings container;
 
 
         public SettingsImpl(bool isRoaming = false) {
             this.IsRoamingProfile = isRoaming;
-            this.container = this.IsRoamingProfile
-                ? ApplicationData.Current.RoamingSettings
-                : ApplicationData.Current.LocalSettings;
+            this.container = IsolatedStorageSettings.ApplicationSettings;
         }
 
 
         public override bool Contains(string key) {
-            return this.container.Values.ContainsKey(key);
+            return this.container.Contains(key);
         }
 
 
         protected override void NativeClear() {
             var keys = this.container
-                .Values
                 .Where(x => this.ShouldClear(x.Key))
                 .Select(x => x.Key)
                 .ToList();
 
             foreach (var key in keys)
-                this.container.Values.Remove(key);
+                this.container.Remove(key);
         }
 
 
         protected override object NativeGet(Type type, string key) {
-            return this.container.Values[key];
+            return this.container[key];
         }
 
 
         protected override void NativeSet(Type type, string key, object value) {
-            this.container.Values[key] = value;
+            this.container[key] = value;
         }
 
 
         protected override void NativeRemove(string key) {
-            this.container.Values.Remove(key);
+            this.container.Remove(key);
         }
 
 
         protected override IDictionary<string, string> NativeValues() {
-            return this.container
-                .Values
-                .ToDictionary(
-                    x => x.Key,
-                    x => x.Value.ToString()
-                );
+            return this.container.ToDictionary(
+                x => x.Key,
+                x => x.Value.ToString()
+            );
         }
     }
 }
