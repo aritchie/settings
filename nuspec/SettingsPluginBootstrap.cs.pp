d1807ade71899af63f013333836718f19c05fc22
Allan Ritchie <allan.ritchie@gmail.com>
2016-03-10 10:25:04 -0500

    Delete some junk

--- nuspec/SettingsPluginBootstrap.cs.pp
+++
@@ -1,9 +0,0 @@
-using Cirrious.CrossCore.Plugins;
-
-namespace $rootnamespace$.Bootstrap
-{
-    public class SettingsPluginBootstrap
-        : MvxPluginBootstrapAction<Acr.MvvmCross.Plugins.Settings.PluginLoader>
-    {
-    }
-}
--- nuspec/TouchSettingsPluginBootstrap.cs.pp
+++
@@ -1,9 +0,0 @@
-using Cirrious.CrossCore.Plugins;
-
-namespace $rootnamespace$.Bootstrap
-{
-    public class SettingsPluginBootstrap
-        : MvxLoaderPluginBootstrapAction<Acr.MvvmCross.Plugins.Settings.PluginLoader, Acr.MvvmCross.Plugins.Settings.Touch.Plugin>
-    {
-    }
-}
