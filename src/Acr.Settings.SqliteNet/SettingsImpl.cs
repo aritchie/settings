73eea68b3e2b1393f1f6f250c25602e4c425892c
Allan Ritchie <allan.ritchie@gmail.com>
2017-09-28 21:25:05 -0400

    netstandard

---
+++ src/Acr.Settings.SqliteNet/SettingsImpl.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Acr.Settings
+{
+    public class SettingsImpl : AbstractSettings
+    {
+        public override bool Contains(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        protected override object NativeGet(Type type, string key)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        protected override void NativeSet(Type type, string key, object value)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        protected override void NativeRemove(string[] keys)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        protected override IDictionary<string, string> NativeValues()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
