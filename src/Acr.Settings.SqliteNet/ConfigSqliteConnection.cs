67c2eede4bd90ee92a7123680e0bad974d887fa2
Allan Ritchie <allan.ritchie@gmail.com>
2017-09-29 11:59:20 -0400

    minor

---
+++ src/Acr.Settings.SqliteNet/ConfigSqliteConnection.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using SQLite;
+
+
+namespace Acr.Settings
+{
+    public class ConfigSqliteConnection : SQLiteConnectionWithLock
+    {
+        public ConfigSqliteConnection(string databaseName)
+            : base(
+                new SQLiteConnectionString(databaseName, true),
+                SQLiteOpenFlags.Create | SQLiteOpenFlags.FullMutex | SQLiteOpenFlags.ReadWrite
+            )
+        {
+        }
+
+
+        public void Init() => this.CreateTable<CacheItem>();
+        public TableQuery<CacheItem> CacheItems => this.Table<CacheItem>();
+    }
+}
---
+++ src/Acr.Settings.SqliteNet/LocalCache.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace Acr.Settings
+{
+    public class LocalCache
+    {
+        IDictionary<string, CacheItem> values;
+        readonly ConfigSqliteConnection conn;
+
+
+        public LocalCache(ConfigSqliteConnection conn)
+        {
+            this.conn = conn;
+            this.values = new Dictionary<string, CacheItem>();
+        }
+
+
+        public void Init()
+        {
+            this.conn.Init();
+            this.values = this.conn
+                .CacheItems
+                .ToList()
+                .ToDictionary(
+                    x => x.Key,
+                    x => x
+                );
+        }
+
+        public object Get(string key)
+        {
+            using (this.conn.Lock())
+            {
+                if (this.values.ContainsKey(key))
+                    return this.values[key].GetValue();
+
+                return null;
+            }
+        }
+
+
+        public void Set(string key, object value)
+        {
+            using (this.conn.Lock())
+            {
+                if (this.values.ContainsKey(key))
+                {
+                    var item = this.values[key];
+                    item.SetValue(value);
+                    this.conn.Update(item);
+                }
+                else
+                {
+                    var item = new CacheItem { Key = key };
+                    item.SetValue(value);
+                    this.values.Add(key, item);
+                    this.conn.Insert(item);
+                }
+            }
+        }
+
+
+        public bool Remove(string key)
+        {
+            using (this.conn.Lock())
+            {
+                if (!this.values.ContainsKey(key))
+                    return false;
+
+                var item = this.values[key];
+                this.conn.Delete(item);
+                return true;
+            }
+        }
+
+
+        public void Clear()
+        {
+            using (this.conn.Lock())
+            {
+                try
+                {
+                    this.conn.BeginTransaction();
+
+                    foreach (var item in this.values.Values)
+                        this.conn.Delete(item);
+
+                    this.conn.Commit();
+                    this.values.Clear();
+                }
+                finally
+                {
+                    this.conn.Rollback();
+                }
+            }
+        }
+
+
+        public bool Contains(string key)
+        {
+            using (this.conn.Lock())
+                return this.values.ContainsKey(key);
+        }
+    }
+}
