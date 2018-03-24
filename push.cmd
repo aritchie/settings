76cb3038af397349e5d6d3ca0ed3869331637ada
Allan Ritchie <allan.ritchie@gmail.com>
2018-03-24 14:44:28 -0400

    fix deployment script

--- push.cmd
+++ push.cmd
@@ -1,3 +1,5 @@
 @echo off
-nuget push *.nupkg -Source https://www.nuget.org/api/v2/package
+nuget push .\Acr.Settings\bin\Release\*.nupkg -Source https://www.nuget.org/api/v2/package
+nuget push .\Acr.Settings\bin\Release\*.nupkg -Source https://www.myget.org/F/acr/api/v2/package
+del .\Acr.Settings\bin\Release\*.nupkg
 pause
